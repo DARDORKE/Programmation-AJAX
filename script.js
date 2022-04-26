473590bb0719e5de3e32fa20beb75c67cc14de85
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 14:11:58 +0200

    Headers AJAX

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-const tbody = document.getElementById('tbody');
-const students = [];
+let users = []
 
-const createLine = (tbody, student) => {
-    const newRow   = tbody.insertRow();
-    let counter = 0
-    for (const key in student) {
-        const newCell  = newRow.insertCell(counter);
-        const newText  = document.createTextNode(student[key]);
-        newCell.appendChild(newText);
-        counter++
-    }
-}
-
-const sendForm = (firstName, lastName, grade, numberClass) => {
-    const student = {id: '', firstName, lastName, grade, numberClass};
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://reqres.in/api/users');
-
-    xhr.addEventListener('readystatechange', () => {
-        if(xhr.readyState === 4) {
-            if (xhr.status === 201) {
-                const data = JSON.parse(xhr.response);
-                student.id = data.id;
-                students.push(student);
-                createLine(tbody, student);
+// fonction pour récupérer la liste des utilisateurs
+const getList = (token) => {
+    const newXhr = new XMLHttpRequest();
+    newXhr.open('GET', 'https://reqres.in/api/users?page=1');
+    newXhr.setRequestHeader('Authorization', token);
+    newXhr.addEventListener('readystatechange', () => {
+        if(newXhr.readyState === 4) {
+            if (newXhr.status === 200) {
+                users = JSON.parse(newXhr.response).data;
+                console.log(users)
             } else {
-                alert('Erreur')
+                alert('Erreur user')
             }
         }
     });
+    newXhr.send();
+}
 
-    xhr.send(student);
+// Action au click sur le formulaire
+const sendForm = (email, password) => {
+    setTimeout(() => {
+        const token = 'ffghthfzszsz654412dsvcds';
+        getList(token);
+    }, 2000)
 }
